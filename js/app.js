7c31c988806d1b1fd4a4b71ce58d84cd040d6f2a
OsmanliFarid <osmanliferid686@gmail.com>
2025-04-28 15:11:18 +0400

    js create

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const Carts = document.querySelector("#Carts")
+const endpoint = "https://dummyjson.com/products"
+
+
+fetch(endpoint).then((item) =>{
+    return item.json()
+    
+    
+}).then((data) =>{
+    data.products.forEach(element => {
+        console.log(element);
+        
+        Carts.innerHTML += `<div class="col-md-4">
+      <div class="card h-100">
+        <img src="${element.images}" class="card-img" alt="">
+        <div class="card-body text-center">
+          <h5 class="card-title">${element.title}</h5>
+          <p class="card-text">${element.description}</p>
+          <p class="text-success fw-bold">2.50 AZN</p>
+          <a href="#" class="btn btn-success">Səbətə əlavə et</a>
+        </div>
+      </div>
+    </div>`
+        
+    });
+    
+})
